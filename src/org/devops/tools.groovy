52bce3cbf459c1b39be3214f0a58284de008bb49
Administrator <admin@example.com>
2023-04-28 15:02:30 +0800

    'test'

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -14,11 +14,11 @@ def PrintMes(value,color){
 
 
 def Docker_Build(){
-              //sh 'docker build -t mrjiangguoqing/jgq:${GIT_SHA:0:7}-$(date +%s) .'
+              sh 'docker build -t mrjiangguoqing/jgq:${developmentTag} .'
               //sh 'docker build -t mrjiangguoqing/gojgq-dev-${GIT_BRANCH}-${GIT_SHA:0:7}-$(date +%s):v5 .'
               sh 'docker login  -u mrjiangguoqing -p jgq123'
               sh 'docker images'              
-              sh 'docker push mrjiangguoqing/jgq:${GIT_SHA:0:7}-$(date +%s)'
+              sh 'docker push mrjiangguoqing/jgq:${developmentTag}'
               //sh 'docker push mrjiangguoqing/gojgq-dev-${GIT_BRANCH}-${GIT_SHA:0:7}-$(date +%s):v5'
               sh '''
               echo "you did it!!!!!!!  yes!!"
