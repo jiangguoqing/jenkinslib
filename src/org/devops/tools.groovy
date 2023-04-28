2c62f86570433cb7d083fdb9c0d59669dc9b7930
Administrator <admin@example.com>
2023-04-28 15:37:27 +0800

    'test'

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -14,7 +14,8 @@ def PrintMes(value,color){
 
 
 def Docker_Build(tag){
-              sh 'echo ${tag}'
+              println(tag)
+              sh 'ech0'
               sh 'echo this'
               sh 'docker build -t mrjiangguoqing/jgq:${tag} .'
               //sh 'docker build -t mrjiangguoqing/gojgq-dev-${GIT_BRANCH}-${GIT_SHA:0:7}-$(date +%s):v5 .'
