3958c42d0030771520acfe649e30c6c674b36773
littlej <mrjiangguoqing@Gmail.com>
2023-04-28 14:08:59 +0800

    Update tools.groovy

--- src/org/devops/tools.groovy
+++ src/org/devops/tools.groovy
@@ -17,6 +17,7 @@ def Docker_Build(){
               sh 'docker build -t mrjiangguoqing/gojgq-dev-${GIT_BRANCH}-${GIT_SHA:0:7}-$(date +%s):v5 .'
               sh 'docker login  -u mrjiangguoqing -p jgq123'
               sh 'docker push mrjiangguoqing/gojgq-dev-${GIT_BRANCH}-${GIT_SHA:0:7}-$(date +%s):v5'
+              sh 'docker images'
               sh '''
               echo "you did it!!!!!!!  yes!!"
               '''
